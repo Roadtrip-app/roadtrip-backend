0479eb29b2fe14b439893b2a6e4b7495934560a4
Wolfy Fiorini <jonwolffiorini@gmail.com>
2023-06-04 14:35:21 -0600

    test

--- routes/pinRoutes.js
+++ routes/pinRoutes.js
@@ -30,8 +30,8 @@ router.post('/create', async (req, res) => {
 router.patch('/:id', async (req, res) => {
 	// Check if user is authorized first
 	try {
-		const updated = await Pin.findByIdAndUpdate(req.params.id, req.body, {new: true})
-		res.send(updated)
+		const updated = await Pin.findByIdAndUpdate(req.params.id, req.body, {new: true});
+		res.send(updated);
 	} catch (err) {
 		console.log(err);
 		res.send("error occured").status(400);
