6a2b8dc880cd77619176d3a23b5d2ac896b0d571
Wolfy319 <jonwolffiorini@gmail.com>
2023-06-13 16:22:35 -0600

    Set up ROI generation

--- assets/helpers/LocationGenerator.js
+++ assets/helpers/LocationGenerator.js
@@ -1,71 +1,58 @@
 import { RouteNode } from "./RouteNode";
-import {dist, midpoint, midpoint} from "./MapHelper";
+import {dist, calcPoint} from "./MapHelper";
 
 // Function that takes a route and finds locations to visit along that route
-export function findPOIs(filter, routeCoords) {
-	const regionsOfInterest = generateROI(routeCoords[0], 
-								   routeCoords[routeCoords.length - 1], 
-								   routeCoords.slice(routeCoords[0], routeCoords.length));
+export function findPOIs(filter, routeCoords, isInKm, numStops = null) {
+	const {routeStart, routeDistance} = createRouteLinkedList(routeCoords);
+	const regionsOfInterest = generateROIs(routeStart, routeDistance, isInKm, numStops);
 	let pointsOfInterest = []
 	regionsOfInterest.forEach(region => {
+		// DB call
 		// API call
+
+		// look for user-generated ones first, if none are rated higher than 3 then use API
 	})
 }
 
-function generateROIs(start, end, route) {
-	// Make route into doubly linked list
-	const head = new RouteNode(start.latitude, start.longitude);
-	const tail = new RouteNode(end.latitude, end.longitude);
-	let current = head;
-	route.forEach(element => {
-		current.next = new RouteNode(element.latitude, element.longitude, null, current);
-		current = current.next;
-	});
-	current.next = tail;
-	tail.prev = current;
-
-	let currentNode = head;
-	const thresh = 10; // The max distance a bounding box can cover
-	const minDist = 10; // The min distance a point must be away from the prev points
-	const offset = 10; // The buffer around the points to add to bound box
-	let currDist = 0;
+function generateROIs(routeHead, totalDistance, isInKm, numStops) {
+	// Add ROI to start 
 	let ROIs = []
+	ROIs.append([currentNode.lat, currentNode.lon]);
+	// Divide route by number of stops requested, subtract 1 for the start ROI
+	const distBetweenROI = totalDistance / (numStops - 1); 
+	
+	// Travel through route
+	let currentNode = routeHead;
+	let currDist = 0;
 	while(currentNode.next) {
-		const distanceToNext = dist(currentNode, currentNode.next)
-		// If there is too much distance from current node to next
-		if (distanceToNext > thresh) {
-			// Create a new node inbetween current and next route node
-			const midpointCoordinates = calcMidpoint(currentNode, currentNode.next);
-			const midpoint = new RouteNode(midpointCoordinates[0], midpointCoordinates[1], 
-															currentNode.next, currentNode);
-			currentNode.next.prev = midpoint;
-			currentNode.next = midpoint;
-			continue
-		} else {
-			// If the next node is far enough away from the current
-			if(distanceToNext > minDist) {
-				// Add bbox to array
-				ROIs.push([Math.min(currentNode.long - offset, currentNode.next.long - offset), 
-						  Math.max(currentNode.long + offset, currentNode.next.long + offset),
-						  Math.min(currentNode.lat - offset, currentNode.next.lat - offset),
-						  Math.max(currentNode.lat + offset, currentNode.next.lat + offset)])
-			} else {
-				// If there are more nodes after, just remove next node
-				if (current.next.next) {
-					current.next.prev = null
-					current.next = current.next.next
-					current.next.prev = current
-					continue
-				} else {
-					// If the next node is a tail node, then just draw the bbox even if it's small
-					ROIs.push([Math.min(currentNode.long - offset, currentNode.next.long - offset), 
-						Math.max(currentNode.long + offset, currentNode.next.long + offset),
-						Math.min(currentNode.lat - offset, currentNode.next.lat - offset),
-						Math.max(currentNode.lat + offset, currentNode.next.lat + offset)])
-				}
-			}
+		// Find distance to the next route node
+		const distances = dist(currentNode, currentNode.next)
+		const distanceToNext = isInKm ? distances[0] : distances[1];
+		// Add it to the distance from the previous ROI to current node
+		currDist += distanceToNext
+		// While the distance traveled is greater than the set distance
+		while(currDist >= distBetweenROI) {
+			// Keep adding ROIs in between current and next point
+			currDist -= distBetweenROI;
+			nextROI = calcPoint(currentNode, currentNode.next, distanceToNext - currDist);
+			ROIs.append(nextROI);
 		}
-	}
-
+		// Continue to next point
+		currentNode = currentNode.next
+	} 
+	return ROIs
 }
 
+// Make route into doubly linked list and find the route's total distance
+function createRouteLinkedList(route) {
+	let head = new RouteNode();
+	let current = head;
+	let totalDistance = 0;
+	route.forEach(element => {
+		totalDistance += element.distance
+		current.next = new RouteNode(element.latitude, element.longitude, null, prev = current);
+		current = current.next;
+	});
+	head = head.next
+	return [head, totalDistance]
+}
--- assets/helpers/MapHelper.js
+++ assets/helpers/MapHelper.js
@@ -1,9 +1,52 @@
-export const dist = (point1, point2, miles = true) => {
+const toRadians = 0.017453292519943295; // Math.PI / 180, converts from degrees to radians
+const cos = Math.cos;
+const sin = Math.sin;
+const EARTH_RADIUS = 6378.137;
 
-	const p = 0.017453292519943295;  // Math.PI / 180, converts degrees to radians
-	return Math.acos(Math.sin(lat1) * Math.sin(lat2))
+// Calculate distance between two latitude/longitude coordinates
+// Taken from https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+export const dist = (point1, point2) => {
+	// Code taken from 
+	// by user Salvador Dali
+	const [lat1, lon1] = pointToRadians(point1);
+	const [lat2, lon2] = pointToRadians(point2);
+	
+  	const a = 0.5 - cos(lat2 - lat1)/2 + 
+        cos(lat1) * cos(lat2) * 
+        (1 - cos(lon2 - lon1)) / 2;
+
+	const km = 2 * EARTH_RADIUS * Math.asin(Math.sqrt(a));
+	const mi = km * 0.62137
+  	return [km, mi]
 }
 
-export const calcMidpoint = (point1, point2) => {
+// Generate latitude and longitude that is in between point1 and point2 and is a certain distance away from point1
+// Taken from https://stackoverflow.com/questions/7222382/get-lat-long-given-current-point-distance-and-bearing
+export const calcPoint = (point1, point2, distance) => {
+	const [lat1, lon1] = pointToRadians(point1);
+	const [lat2, lon2] = pointToRadians(point2);
+	// Find the angle towards point2
+	const bearing = calcBearing(lat1, lon1, lat2, lon2) * toRadians;
+	// Compensate for earth's radius
+	distance /= EARTH_RADIUS;
 
+	// Calculate new lat/lon
+	const latInRad = (Math.asin(sin(lat1) * cos(distance) + cos(lat1) * sin(distance)* cos(bearing)));
+	const lonInRad = (lat1 + Math.atan2(sin(bearing) * sin(distance) * cos(lat1), cos(distance) - sin(lat1) * sin(latInRad)));
+
+	return [latInRad / toRadians, lonInRad / toRadians]
+}
+
+// Calculate the bearing from one set of coordinates to the next
+// Taken from https://stackoverflow.com/questions/46590154/calculate-bearing-between-2-points-with-javascript
+const calcBearing = (lat1, lon1, lat2, lon2) => {
+	y = sin((lon2 - lon1)) * cos(lat2);
+	x = cos(lat1) * sin(lat2) -
+		  sin(lat1) * cos(lat2) * cos((lon2 - lon1));
+	bearing = Math.atan2(y, x);
+	return bearing
+}
+
+const pointToRadians = (point) => {
+	return [point[0] * toRadians, point[1] * toRadians]
 }
