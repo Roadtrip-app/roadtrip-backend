44244ee70675d2b1c058ff65eea27d4870257b12
Wolfy319 <jonwolffiorini@gmail.com>
2023-06-22 16:42:57 -0600

    wrote tests for ROI generation

--- __tests__/routeGeneratorTest.js
+++ __tests__/routeGeneratorTest.js
@@ -3,23 +3,34 @@ const {generateROIs, findPOIs} = require('../assets/helpers/LocationGenerator');
 const request = require('supertest');
 const app = require('../server');
 const {getRoute} = require('../assets/objects/setupTests')
+const {fakeRoute} = require('../assets/objects/testObjects');
 let testRoute;
 describe('Test route generation', () => { 
 
 	beforeAll(async () => {
 		testRoute = await getRoute();
 	})
-    
-	// it('tests google maps directions API', async () => {
-    //     let res = await request(app).get('/generate-route/?origin=Denver&destination=Littleton');
-    //     console.log(res)
-    // })
 
 	describe('Test ROI generation', () => {
-		it('should create a list of 10 ROIs', async () => {
-			const regions = generateROIs(testRoute, 10);
-			console.log(regions)
-			expect(regions.length).toEqual(10)
+		it('should create a list of 10 ROIs', () => {
+			const regions = generateROIs(fakeRoute, 10);
+			expect(regions.length).toEqual(10);
 		});
 	}); 
+
+	describe('Test ROI generation from API', () => {
+		it('should create a list of 5 ROIs', () => {
+			const regions = generateROIs(testRoute, 5);
+			expect(regions.length).toEqual(5);
+		})
+
+		it('checks that API and mocked routes generate similar ROIs', async () => {
+			const mockedRegions = await generateROIs(fakeRoute, 5);
+			const apiRegions = await generateROIs(testRoute, 5);
+			for(let i = 0; i < mockedRegions.length; i++) {
+				expect(mockedRegions[i][0] - apiRegions[i][0]).toBeLessThan(0.02)
+				expect(mockedRegions[i][1] - apiRegions[i][1]).toBeLessThan(0.02)
+			}
+		})
+	})
 })
