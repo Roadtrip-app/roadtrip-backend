98d4cbd4a8dce1ee75053ba8e1f28971764c256c
Wolfy319 <jonwolffiorini@gmail.com>
2023-06-25 12:53:57 -0600

    created POI generation tests

--- __tests__/routeGeneratorTest.js
+++ __tests__/routeGeneratorTest.js
@@ -3,7 +3,7 @@ const {generateROIs, findPOIs} = require('../assets/helpers/LocationGenerator');
 const request = require('supertest');
 const app = require('../server');
 const {getRoute} = require('../assets/objects/setupTests')
-const {fakeRoute} = require('../assets/objects/testObjects');
+const {fakeRoute, testROIs} = require('../assets/objects/testObjects');
 let testRoute;
 describe('Test route generation', () => { 
 
@@ -12,25 +12,44 @@ describe('Test route generation', () => {
 	})
 
 	describe('Test ROI generation', () => {
-		it('should create a list of 10 ROIs', () => {
-			const regions = generateROIs(fakeRoute, 10);
-			expect(regions.length).toEqual(10);
-		});
+		describe('Test ROI generation from mocked route', () => {
+			it('should create a list of 10 ROIs', () => {
+				const regions = generateROIs(fakeRoute, 10);
+				expect(regions.length).toEqual(10);
+			});
+		})
+		
+		describe('Test ROI generation from API', () => {
+			it('should create a list of 5 ROIs', () => {
+				const regions = generateROIs(testRoute, 5);
+				expect(regions.length).toEqual(5);
+			})
+	
+			it('checks that API and mocked routes generate similar ROIs', async () => {
+				const mockedRegions = generateROIs(fakeRoute, 5);
+				const apiRegions = generateROIs(testRoute, 5);
+				for(let i = 0; i < mockedRegions.length; i++) {
+					expect(mockedRegions[i][0] - apiRegions[i][0]).toBeLessThan(0.02)
+					expect(mockedRegions[i][1] - apiRegions[i][1]).toBeLessThan(0.02)
+				}
+			})
+		})
 	}); 
 
-	describe('Test ROI generation from API', () => {
-		it('should create a list of 5 ROIs', () => {
-			const regions = generateROIs(testRoute, 5);
-			expect(regions.length).toEqual(5);
+	describe('Test POI generation', () => {
+		const results = findPOIs(testROIs, 1000);
+		it('should return a single location per ROI', () => {
+			expect(results.length === 10);
 		})
 
-		it('checks that API and mocked routes generate similar ROIs', async () => {
-			const mockedRegions = await generateROIs(fakeRoute, 5);
-			const apiRegions = await generateROIs(testRoute, 5);
-			for(let i = 0; i < mockedRegions.length; i++) {
-				expect(mockedRegions[i][0] - apiRegions[i][0]).toBeLessThan(0.02)
-				expect(mockedRegions[i][1] - apiRegions[i][1]).toBeLessThan(0.02)
+		it('will only return points within the radius of an ROI', () => {
+			for(location in results) {
+				expect(location.properties.dist <= 1000)
 			}
 		})
+
+		// it('should return POIs that fit the criteria', () => {
+
+		// })
 	})
 })
