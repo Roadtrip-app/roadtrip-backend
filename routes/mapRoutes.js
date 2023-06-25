167c3dfbb12847de463c5647a8aebe54e4b95e6d
Wolfy319 <jonwolffiorini@gmail.com>
2023-06-25 12:53:08 -0600

    add temp route for POI generating

--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {Client} = require('@googlemaps/google-maps-services-js');
 const client = new Client({});
-
+const {testROIs} = require('../assets/objects/testObjects');
+const {findPOIs} = require('../assets/helpers/LocationGenerator');
 router.get('/generate-route', async (req, res) => {
-    console.log(req.query)
     const params = {
         origin: req.query.origin,
         destination: req.query.destination,
@@ -21,4 +21,8 @@ router.get('/generate-route', async (req, res) => {
     })
 })
 
+router.get('/find-places', async (req, res) => {
+    const points = await findPOIs(testROIs, 2000)
+    res.send(points)
+})
 module.exports = router;
